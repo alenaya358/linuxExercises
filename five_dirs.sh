c566215b57b6fc65afc21e33ef95b5d9e7728f6c
Ziyi Yang <zyang622@wisc.edu>
2023-03-08 18:44:45 -0600

    Adjust code

--- five_dirs.sh
+++ five_dirs.sh
@@ -4,6 +4,21 @@ for i in {1..5}; do
   mkdir five/dir$i
   for j in {1..4}; do
       touch five/dir$i/file$j
- 
+      if [[ "$j" =~ ^[01] ]]; then
+	  echo "1" >> five/dir$i/file$j
+      elif [[ "$j" =~ ^[02] ]]; then
+          echo "2" >> five/dir$i/file$j
+          echo "2" >> five/dir$i/file$j
+      elif [[ "$j" =~ ^[03] ]]; then
+	  echo "3" >> five/dir$i/file$j
+	  echo "3" >> five/dir$i/file$j
+	  echo "3" >> five/dir$i/file$j
+      else
+	  echo "4" >> five/dir$i/file$j
+	  echo "4" >> five/dir$i/file$j
+	  echo "4" >> five/dir$i/file$j
+	  echo "4" >> five/dir$i/file$j
+
+      fi
   done
 done
