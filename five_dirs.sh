a114844b6ca8826701a206b1c249b3fd4dc41491
Alena Zheng <jzheng235@wisc.edu>
2023-03-10 21:46:07 -0600

    simplified five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -2,23 +2,15 @@
 
 mkdir five
 
-for i in {1..5}; do
-  mkdir five/dir$i
-  for j in {1..4}; do
-      touch five/dir$i/file$j
-      if [[ "$j" =~ ^[01] ]]; then
-	  echo "1" >> five/dir$i/file$j
-      elif [[ "$j" =~ ^[02] ]]; then
-          echo "2" >> five/dir$i/file$j
-          echo "2" >> five/dir$i/file$j
-      elif [[ "$j" =~ ^[03] ]]; then
-	  for a in {1..3}; do
-	      echo "3" >> five/dir$i/file$j
-	  done
-      else
-	  for b in {1..4}; do
-	      echo "4" >> five/dir$i/file$j
-	  done
-      fi
+for i in {1..5}
+do
+  mkdir "five/dir$i"
+done
+
+for i in {1..5}
+do
+  for j in {1..4}
+  do
+    yes $j | head -n $j > "five/dir$i/file$j"
   done
 done
